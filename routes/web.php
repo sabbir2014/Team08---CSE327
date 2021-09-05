6ee3f8b0fb83d947bc15ff0aafa9f8a2c40ad5d3
Labiba-Islam <labiba.islam@northsouth.edu>
2021-09-05 18:17:29 +0600

    Created product details page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
@@ -24,10 +25,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',HomeComponent::class);
 
 Route::get('/shop', ShopComponent::class);
+
 Route::get('/cart', CartComponent::class);
+
 Route::get('/checkout', CheckoutComponent::class);
 
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
